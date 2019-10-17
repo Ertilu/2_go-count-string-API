76cebd858e32c54bc5598e561032367087c85e9a
ERTILU <rezaraka00@gmail.com>
2019-10-17 14:15:39 +0700

    create simple server

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<h1>HELLO WORLD</h1>"))
+	})
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<h1>This is homepage</h1>"))
+	})
+	fmt.Println("server running in port 8000")
+	http.ListenAndServe(":8000", nil)
 }
