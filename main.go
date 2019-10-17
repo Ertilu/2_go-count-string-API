7f6f48318db2b7aad4161b558bdedeb8573528bb
ERTILU <rezaraka00@gmail.com>
2019-10-17 14:21:34 +0700

    update simple server

--- main.go
+++ main.go
@@ -12,6 +12,14 @@ func main() {
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<h1>This is homepage</h1>"))
 	})
+	http.HandleFunc("/output", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			input := r.FormValue("input")
+			w.Write([]byte(input))
+			return
+		}
+		http.Error(w, "Bad request", http.StatusBadRequest)
+	})
 	fmt.Println("server running in port 8000")
 	http.ListenAndServe(":8000", nil)
 }
