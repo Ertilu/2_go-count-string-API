bde36f2a0c880e5a2dde6ca1ae52540ce307efe9
ERTILU <rezaraka00@gmail.com>
2019-10-18 13:33:23 +0700

    complete web service for count string

--- main.go
+++ main.go
@@ -1,25 +1,32 @@
 package main
 
 import (
+	c "countWebService/count"
 	"fmt"
 	"net/http"
 )
 
+func homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "HELLO Welcome to count string app. go to /count to start counting a word. And type in form-data to send the input.")
+	http.Error(w, "Bad request", http.StatusBadRequest)
+}
+
+func countController(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		input := r.FormValue("input")
+		vowels, consonant := c.Count(input)
+		fmt.Fprintf(w, vowels+"\n"+consonant)
+	}
+	http.Error(w, "Bad request try POST method to start count a string", http.StatusBadRequest)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>HELLO WORLD</h1>"))
-	})
-	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>This is homepage</h1>"))
-	})
-	http.HandleFunc("/output", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" {
-			input := r.FormValue("input")
-			w.Write([]byte(input))
-			return
-		}
-		http.Error(w, "Bad request", http.StatusBadRequest)
-	})
-	fmt.Println("server running in port 8000")
-	http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/", homepage)
+
+	http.HandleFunc("/count", countController)
+
+	port := "8000"
+
+	fmt.Println("server running in port " + port)
+	http.ListenAndServe(":"+port, nil)
 }
